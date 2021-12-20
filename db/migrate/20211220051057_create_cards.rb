530316b48739c40e6f750752290b230961b5b88b
eunbiggabi <eunbiggabi84@gmail.com>
2021-12-20 15:12:49 +1000

    add theme to card

---
+++ db/migrate/20211220051057_create_cards.rb
@@ -0,0 +1,14 @@
+class CreateCards < ActiveRecord::Migration[6.1]
+  def change
+    create_table :cards do |t|
+      t.string :name
+      t.string :company
+      t.string :title
+      t.string :email
+      t.text :message
+      t.references :theme, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
