f61e8f251cf71aa8a8c8c8c512cd0be455f3303f
eunbiggabi <eunbiggabi84@gmail.com>
2021-12-21 16:32:04 +1000

    rails db:seed is not working

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_12_21_060725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "cards", force: :cascade do |t|
+    t.string "name"
+    t.string "company"
+    t.string "title"
+    t.string "email"
+    t.text "message"
+    t.bigint "theme_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["theme_id"], name: "index_cards_on_theme_id"
+  end
+
+  create_table "themes", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "cards", "themes"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+themes = Theme.create([
+  {
+    name: 'light',
+  },
+  {
+    name: 'dark',
+  },
+  {
+    name: 'colorful',
+  }
+])
+
+cards = Card.create([
+  {
+    name: "Kyu",
+    company: "Google",
+    title: "Software developer",
+    email: "kyu@email.com",
+    text: "go for it"
+    themes: themes.first
+  },
+  {
+    name: "Bong",
+    company: "Samsung",
+    title: "Admin",
+    email: "bong@email.com",
+    text: "let's go"
+    themes: themes.second
+  }
+])
